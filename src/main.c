34598d3ea86784d6d87734061cd33c3726a45767
Adel Khial <adel.khial@gmail.com>
2017-10-27 15:10:28 +0100

    Fixed description.

--- src/main.c
+++ src/main.c
@@ -1,15 +1,15 @@
 /* *********************************************************************************** *
- * Femto is a rudimentary text viewing program capable of searching    				*
- * and word replacement.                                                    			*
- *                                                                       			*
- * THE PROGRAM IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED		*
- * OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF				*
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  THE ENTIRE RISK AS TO THE	*
- * QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU.  SHOULD THE PROGRAM PROVE    	*
- * DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION. 	*
- *			                                                         		 		*
- * main.c                                                                 			*
- * Author: Adel Khial                    	    									*
+ * Femto is a rudimentary text viewing program capable of searching                    *
+ * and word replacement.                                                               *
+ *                                                                                     *
+ * THE PROGRAM IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED      *
+ * OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF                *
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE    *
+ * QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU.  SHOULD THE PROGRAM PROVE       *
+ * DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.    *
+ *                                                                                     *
+ * main.c                                                                              *
+ * Author: Adel Khial                                                                  *
  * *********************************************************************************** */
 
 #include <conio.h>
